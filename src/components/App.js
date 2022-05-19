0956e63916d90cd2434b049039fb40a3caf61f8e
Hidnish <hideonishimura@live.it>
2022-05-19 13:49:40 +0200

    Complete CRUD functinalities setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
@@ -9,16 +9,18 @@ import Header from "./Header";
 import history from '../history';
 
 const App = () => {
-    //1
+    //1 //2
     return (
         <div className="ui container">
             <Router history={history}>
-                <Header></Header>
-                <Route path="/" exact={true} component={StreamList} />
-                <Route path="/streams/new" component={StreamCreate} />
-                <Route path="/streams/edit/:id" component={StreamEdit} />
-                <Route path="/streams/delete/:id" component={StreamDelete} />
-                <Route path="/streams/show" component={StreamShow} />
+                <Header />
+                <Switch>
+                    <Route path="/" exact={true} component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit/:id" component={StreamEdit} />
+                    <Route path="/streams/delete/:id" component={StreamDelete} />
+                    <Route path="/streams/:id" component={StreamShow} />
+                </Switch>
             </Router>
         </div>
     );
@@ -32,3 +34,5 @@ export default App;
 // you can put as many ':any/:anyvar/:anything' variables in the URL that will be converted as props.match.params in the component={}
 
 // <Route></Route> passes a bunch of props to the component={} by default
+
+//2 <Switch></Switch> looks at different routes and selects only one
